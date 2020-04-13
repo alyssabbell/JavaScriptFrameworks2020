5497378589a8344ef7a266a64be35c0fb0ecf9d1
Alyssa Bell <alyssa.bell@troyweb.com>
2020-04-13 17:18:16 -0400

    completed exercise 13

--- exercises/13-class-ajax/src/App.js
+++ exercises/13-class-ajax/src/App.js
@@ -1,34 +1,31 @@
 import React, { Component } from "react";
 import axios from "axios";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 /** Convert me into a class component  */
 
 class App extends Component {
 
   state = {
-    message: "",
-    error: "",
+    quote: "",
+    hasError: false,
+    isLoading: true
   };
 
   componentDidMount() {
-    axios("https://ron-swanson-quotes.herokuapp.com/v2/quotes", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(resp => this.setState({ message: resp.data }))
-      // .catch(error => console.log(error))
-      .catch(error = this.setState({ error: error.data }));
+    axios("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+      .then(response => this.setState({ quote: response.data }))
+      .catch(error => this.setState({ hasError: true }))
+      .then(() => { this.setState({ isLoading: false }) })
   };
 
   render() {
     return (
       <>
-        <ErrorBoundary error={this.state.error}>
-          <div className="App">
-            <h1>{this.state.message}</h1>
-          </div>
-        </ErrorBoundary >
+        <div className="App">
+          <h1>{this.state.quote}</h1>
+          {this.state.isLoading && <p>Loading ...</p>}
+          {this.state.hasError && <p>There is an error</p>}
+        </div>
       </>
     );
   }
